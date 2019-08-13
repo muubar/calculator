63ac5659976518444f472f74f9be97b41d504b3f
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2019-08-13 05:17:36 +0200

    implement overflow auto-scroll

--- src/index.js
+++ src/index.js
@@ -6,61 +6,68 @@ import './index.css';
 class Calculator extends React.Component {
   handleClick = (e) => {
     const text = e.target.innerText;
+    const displayElems = Array.from(document.getElementsByClassName('display-elem'));
+
     if (this.state.isResult) {
       if (['%', '/', '*', '-', '+'].includes(text)) {
-        return this.setState((state) => ({ formula: `${state.formula}${text} `, val: '', isResult: false }))
+        return this.setState((state) => ({ formula: `${state.formula} ${text} `, val: '', isResult: false }), updateOverflow)
       }
-      else return this.setState({ formula: '', val: '', isResult: false });
+      else return this.setState({ formula: '', val: '', isResult: false }, updateOverflow);
     }
 
     else if (Number.isInteger(Number(text))) {
       if (this.state.val === '0' || this.state.val === '-0') {
         if (text === '0') return;
-        else return this.setState((state) => ({ val: state.val[0] === '-' ? '-' + text : text }));
+        else return this.setState((state) => ({ val: state.val[0] === '-' ? '-' + text : text }), updateOverflow);
       }
-      else return this.setState((state) => ({ val: state.val + text }));
+      else return this.setState((state) => ({ val: state.val + text }), updateOverflow);
     }
 
     else if (text === '.' && !this.state.val.includes('.')) {
-      return this.setState((state) => ({ val: state.val.length === 0 ? '0.' : state.val + '.' }));
+      return this.setState((state) => ({ val: state.val.length === 0 ? '0.' : state.val + '.' }), updateOverflow);
     }
 
     else if (text === 'del') {
-      this.setState((state) => ({ val: state.val.substring(0, state.val.length - 1) }));
+      this.setState((state) => ({ val: state.val.substring(0, state.val.length - 1) }), updateOverflow);
     }
 
     else if (text === '+/-' && this.state.val.length > 0 && this.state.val !== '0') {
-      return this.setState((state) => ({ val: state.val[0] === '-' ? state.val.substring(1) : '-' + state.val }));
+      return this.setState((state) => ({ val: state.val[0] === '-' ? state.val.substring(1) : '-' + state.val }), updateOverflow);
     }
 
-    else if (text === 'AC') return this.setState({ formula: '', val: '' });
+    else if (text === 'AC') return this.setState({ formula: '', val: '' }, updateOverflow);
 
     else if (['%', '/', '*', '-', '+'].includes(text)) {
       if (this.state.val === '') return;
       const num = Number(this.state.val);
       if (Number.isNaN(num)) num = 0;
-      this.setState((state) => ({ formula: `${state.formula}${String(num)} ${text} `, val: '' }));
+      this.setState((state) => ({ formula: `${state.formula}${String(num)} ${text} `, val: '' }), updateOverflow);
     }
 
     else if (text === '=') {
       let num;
       if (this.state.val !== '') {
         num = Number(this.state.val);
-        //if (Number.isNaN(num)) num = 0;
       }
       if (num === undefined && this.state.formula === '') return;
       this.setState((state) => ({
         formula: num === undefined ? String(eval(state.formula.substring(0, state.formula.length - 2))) : String(eval(`${state.formula}${String(num)}`)),
         val: '',
         isResult: true
-      }))
+      }), updateOverflow)
+    };
+
+    function updateOverflow() {
+      displayElems.forEach((elem) => elem.scrollLeft += 9999999);
     }
   }
+
 
   constructor() {
     super()
     this.state = { formula: '', val: '', isResult: false };
   }
+
 
   render() {
     return (
